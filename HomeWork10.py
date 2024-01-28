5ea39e5a0a4985c971a1e07515d2893042345a9e
Antoninashu <toziatkina@gmail.com>
2024-01-28 18:48:03 +0200

    HW

---
+++ HomeWork10.py
@@ -0,0 +1,315 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+#
+# url = 'https://www.mensa.lu/en/mensa/online-iq-test/online-iq-test.html'
+# driver.get(url)
+#
+# # поля
+# q1_element = driver.find_element(By.NAME, 'q1')
+# q1_element.send_keys('r')
+# time.sleep(1)
+#
+# q2_element = driver.find_element(By.NAME, 'q2')
+# q2_element.send_keys('3')
+# time.sleep(1)
+#
+# q3_element = driver.find_element(By.NAME, 'q3')
+# q3_element.send_keys('23')
+# time.sleep(1)
+#
+# q4_element = driver.find_element(By.NAME, 'q4')
+# q4_element.send_keys('5')
+# time.sleep(1)
+#
+# q5_element = driver.find_element(By.NAME, 'q5')
+# q5_element.send_keys('56')
+# time.sleep(1)
+#
+# q6_element = driver.find_element(By.NAME, 'q6')
+# q6_element.send_keys('7')
+# time.sleep(1)
+#
+# q7_element = driver.find_element(By.NAME, 'q7')
+# q7_element.send_keys('88')
+# time.sleep(1)
+#
+# q8_element = driver.find_element(By.NAME, 'q8')
+# q8_element.send_keys('56')
+# time.sleep(1)
+#
+# # радіобатон
+# q9_element = driver.find_element(By.XPATH, '//input[@name="q9" and @value="c"]')
+# q9_element.click()
+# time.sleep(1)
+#
+# q10_element = driver.find_element(By.XPATH, '//input[@name="q10" and @value="b"]')
+# q10_element.click()
+# time.sleep(1)
+#
+# q11_element = driver.find_element(By.XPATH, '//input[@name="q11" and @value="d"]')
+# q11_element.click()
+# time.sleep(1)
+#
+# q12_element = driver.find_element(By.XPATH, '//input[@name="q12" and @value="a"]')
+# q12_element.click()
+# time.sleep(1)
+#
+# q13_element = driver.find_element(By.XPATH, '//input[@name="q13" and @value="c"]')
+# q13_element.click()
+# time.sleep(1)
+#
+# q14_element = driver.find_element(By.XPATH, '//input[@name="q14" and @ value="d"]')
+# q14_element.click()
+# time.sleep(1)
+#
+# q15_element = driver.find_element(By.XPATH, '//input[@name="q15" and @value="a"]')
+# q15_element.click()
+# time.sleep(1)
+#
+# q16_element = driver.find_element(By.XPATH, '//input[@name="q16" and @value="d"]')
+# q16_element.click()
+# time.sleep(1)
+#
+# q17_element = driver.find_element(By.XPATH, '//input[@name="q17" and @value="b"]')
+# q17_element.click()
+# time.sleep(1)
+#
+# q18_element = driver.find_element(By.XPATH, '//input[@name="q18" and @value="c"]')
+# q18_element.click()
+# time.sleep(1)
+#
+# # чекбокс
+# q19_b_element = driver.find_element(By.XPATH, '//input[@name="q19" and @value="b"]')
+# q19_b_element.click()
+# time.sleep(2)
+#
+# q19_e_element = driver.find_element(By.XPATH, '//input[@name="q19" and @value="e"]')
+# q19_e_element.click()
+# time.sleep(2)
+#
+# q20_a_element = driver.find_element(By.XPATH, '//input[@name="q20" and @value="a"]')
+# q20_a_element.click()
+# time.sleep(2)
+#
+# q20_c_element = driver.find_element(By.XPATH, '//input[@name="q20" and @value="c"]')
+# q20_c_element.click()
+# time.sleep(2)
+#
+# q21_c_element = driver.find_element(By.XPATH, '//input[@name="q21" and @value="c"]')
+# q21_c_element.click()
+# time.sleep(2)
+#
+# q21_e_element = driver.find_element(By.XPATH, '//input[@name="q21" and @value="e"]')
+# q21_e_element.click()
+# time.sleep(2)
+#
+# q22_d_element = driver.find_element(By.XPATH, '//input[@name="q22" and @value="d"]')
+# q22_d_element.click()
+# time.sleep(2)
+#
+# q22_e_element = driver.find_element(By.XPATH, '//input[@name="q22" and @value="e"]')
+# q22_e_element.click()
+# time.sleep(2)
+#
+# # радіобатон
+# q23_element = driver.find_element(By.XPATH, '//input[@name="q23" and @value="c"]')
+# q23_element.click()
+# time.sleep(1)
+#
+# q24_element = driver.find_element(By.XPATH, '//input[@name="q24" and @value="a"]')
+# q24_element.click()
+# time.sleep(1)
+#
+# q25_element = driver.find_element(By.XPATH, '//input[@name="q25" and @value="d"]')
+# q25_element.click()
+# time.sleep(1)
+#
+# q26_element = driver.find_element(By.XPATH, '//input[@name="q26" and @value="b"]')
+# q26_element.click()
+# time.sleep(1)
+#
+# q27_element = driver.find_element(By.XPATH, '//input[@name="q27" and @value="c"]')
+# q27_element.click()
+# time.sleep(1)
+#
+# q28_element = driver.find_element(By.XPATH, '//input[@name="q28" and @value="a"]')
+# q28_element.click()
+# time.sleep(1)
+#
+# q29_element = driver.find_element(By.XPATH, '//input[@name="q29" and @value="c"]')
+# q29_element.click()
+# time.sleep(1)
+#
+# q30_element = driver.find_element(By.XPATH, '//input[@name="q30" and @value="b"]')
+# q30_element.click()
+# time.sleep(1)
+#
+# q31_element = driver.find_element(By.XPATH, '//input[@name="q31" and @value="c"]')
+# q31_element.click()
+# time.sleep(1)
+#
+# q32_element = driver.find_element(By.XPATH, '//input[@name="q32" and @value="d"]')
+# q32_element.click()
+# time.sleep(1)
+#
+# q33_element = driver.find_element(By.XPATH, '//input[@name="q33" and @value="a"]')
+# q33_element.click()
+# time.sleep(1)
+
+
+url1 = 'https://test.mensa.no/'
+driver.get(url1)
+
+age_1850 = driver.find_element(By.XPATH, '//button[contains(@class," ageselect_1850")]')
+age_1850.click()
+time.sleep(1)
+
+start_button = driver.find_element(By.ID, 'startTest')
+start_button.click()
+time.sleep(1)
+
+q1_a_button = driver.find_element(By.XPATH, '//div[@id="question_0"]//div[@data-answerid="0"]')
+q1_a_button.click()
+time.sleep(1)
+
+q2_b_button = driver.find_element(By.XPATH, '//div[@id="question_1"]//div[@data-answerid="1"]')
+q2_b_button.click()
+time.sleep(1)
+
+q3_c_button = driver.find_element(By.XPATH, '//div[@id="question_2"]//div[@data-answerid="2"]')
+q3_c_button.click()
+time.sleep(1)
+
+q4_f_button = driver.find_element(By.XPATH, '//div[@id="question_3"]//div[@data-answerid="5"]')
+q4_f_button.click()
+time.sleep(1)
+
+q5_d_button = driver.find_element(By.XPATH, '//div[@id="question_4"]//div[@data-answerid="3"]')
+q5_d_button.click()
+time.sleep(1)
+
+q6_e_button = driver.find_element(By.XPATH, '//div[@id="question_5"]//div[@data-answerid="4"]')
+q6_e_button.click()
+time.sleep(1)
+
+q7_d_button = driver.find_element(By.XPATH, '//div[@id="question_6"]//div[@data-answerid="3"]')
+q7_d_button.click()
+time.sleep(1)
+
+q8_a_button = driver.find_element(By.XPATH, '//div[@id="question_7"]//div[@data-answerid="0"]')
+q8_a_button.click()
+time.sleep(1)
+
+q9_d_button = driver.find_element(By.XPATH, '//div[@id="question_8"]//div[@data-answerid="3"]')
+q9_d_button.click()
+time.sleep(1)
+
+q10_c_button = driver.find_element(By.XPATH, '//div[@id="question_9"]//div[@data-answerid="2"]')
+q10_c_button.click()
+time.sleep(1)
+
+q11_d_button = driver.find_element(By.XPATH, '//div[@id="question_10"]//div[@data-answerid="3"]')
+q11_d_button.click()
+time.sleep(1)
+
+q12_f_button = driver.find_element(By.XPATH, '//div[@id="question_11"]//div[@data-answerid="5"]')
+q12_f_button.click()
+time.sleep(1)
+
+q13_e_button = driver.find_element(By.XPATH, '//div[@id="question_12"]//div[@data-answerid="4"]')
+q13_e_button.click()
+time.sleep(1)
+
+q14_b_button = driver.find_element(By.XPATH, '//div[@id="question_13"]//div[@data-answerid="1"]')
+q14_b_button.click()
+time.sleep(1)
+
+q15_e_button = driver.find_element(By.XPATH, '//div[@id="question_14"]//div[@data-answerid="4"]')
+q15_e_button.click()
+time.sleep(1)
+
+q16_b_button = driver.find_element(By.XPATH, '//div[@id="question_15"]//div[@data-answerid="2"]')
+q16_b_button.click()
+time.sleep(1)
+
+q17_d_button = driver.find_element(By.XPATH, '//div[@id="question_16"]//div[@data-answerid="3"]')
+q17_d_button.click()
+time.sleep(1)
+
+q18_f_button = driver.find_element(By.XPATH, '//div[@id="question_17"]//div[@data-answerid="5"]')
+q18_f_button.click()
+time.sleep(1)
+
+q19_a_button = driver.find_element(By.XPATH, '//div[@id="question_18"]//div[@data-answerid="0"]')
+q19_a_button.click()
+time.sleep(1)
+
+q20_d_button = driver.find_element(By.XPATH, '//div[@id="question_19"]//div[@data-answerid="3"]')
+q20_d_button.click()
+time.sleep(1)
+
+q21_f_button = driver.find_element(By.XPATH, '//div[@id="question_20"]//div[@data-answerid="5"]')
+q21_f_button.click()
+time.sleep(1)
+
+q22_e_button = driver.find_element(By.XPATH, '//div[@id="question_21"]//div[@data-answerid="4"]')
+q22_e_button.click()
+time.sleep(1)
+
+q23_d_button = driver.find_element(By.XPATH, '//div[@id="question_22"]//div[@data-answerid="3"]')
+q23_d_button.click()
+time.sleep(1)
+
+q24_b_button = driver.find_element(By.XPATH, '//div[@id="question_23"]//div[@data-answerid="1"]')
+q24_b_button.click()
+time.sleep(1)
+
+q25_a_button = driver.find_element(By.XPATH, '//div[@id="question_24"]//div[@data-answerid="0"]')
+q25_a_button.click()
+time.sleep(1)
+
+q26_b_button = driver.find_element(By.XPATH, '//div[@id="question_25"]//div[@data-answerid="1"]')
+q26_b_button.click()
+time.sleep(1)
+
+q27_f_button = driver.find_element(By.XPATH, '//div[@id="question_26"]//div[@data-answerid="5"]')
+q27_f_button.click()
+time.sleep(1)
+
+q28_e_button = driver.find_element(By.XPATH, '//div[@id="question_27"]//div[@data-answerid="4"]')
+q28_e_button.click()
+time.sleep(1)
+
+q29_a_button = driver.find_element(By.XPATH, '//div[@id="question_28"]//div[@data-answerid="0"]')
+q29_a_button.click()
+time.sleep(1)
+
+q30_d_button = driver.find_element(By.XPATH, '//div[@id="question_29"]//div[@data-answerid="3"]')
+q30_d_button.click()
+time.sleep(1)
+
+q31_b_button = driver.find_element(By.XPATH, '//div[@id="question_30"]//div[@data-answerid="1"]')
+q31_b_button.click()
+time.sleep(1)
+
+q32_e_button = driver.find_element(By.XPATH, '//div[@id="question_31"]//div[@data-answerid="4"]')
+q32_e_button.click()
+time.sleep(1)
+
+q33_f_button = driver.find_element(By.XPATH, '//div[@id="question_32"]//div[@data-answerid="5"]')
+q33_f_button.click()
+time.sleep(1)
+
+q34_a_button = driver.find_element(By.XPATH, '//div[@id="question_33"]//div[@data-answerid="0"]')
+q34_a_button.click()
+time.sleep(1)
+
+q35_c_button = driver.find_element(By.XPATH, '//div[@id="question_34"]//div[@data-answerid="2"]')
+q35_c_button.click()
+time.sleep(1)
+
+time.sleep(3)
